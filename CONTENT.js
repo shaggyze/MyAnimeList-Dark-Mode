1d66b656851a985874d53a0f0e74b2317ca87345
shaggyze <shaggyze@gmail.com>
2022-07-18 10:57:32 -0700

    Added Chrome Save/Load Settings
    -Added chrome storage for settings, might add firefox or local storage if it's needed/necessary

--- CONTENT.js
+++ CONTENT.js
@@ -1,21 +1,36 @@
-$(document).ready(function () {
-  // listen for messages sent to the tab the content script is running in
-  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    // check to see if the message received is something that needs to be acted on
-    if (request.todo === "changeWidth") {
-      // pull the width data from the message
-      const newWidth = request.newWidth;
-      // set the style attribute of :root to override the MAIN.css value for --ptwidth
-      document.documentElement.style.setProperty("--ptwidth", newWidth + "px");
-	  // send a response to avoid errors in popup.js
-      sendResponse("Width updated");
-    } else if (request.todo === "changeColor") {
-      // pull the width data from the message
-      const newColor = request.newColor;
-      // set the style attribute of :root to override the MAIN.css value for --ptwidth
-      document.documentElement.style.setProperty("--page", newColor);
-	  // send a response to avoid errors in popup.js
-    sendResponse("Color updated");
-    }
-  });
+document.body.onload = function() {
+    chrome.storage.sync.get("newWidth", function(items) {
+        if (!chrome.runtime.error) {
+            console.log(items);
+            document.documentElement.style.setProperty("--ptwidth", items.newWidth + "px");
+        }
+    });
+    chrome.storage.sync.get("colorChanger", function(items) {
+        if (!chrome.runtime.error) {
+            console.log(items);
+            document.documentElement.style.setProperty("--page", items.colorChanger);
+        }
+    });
+}
+
+$(document).ready(function() {
+    // listen for messages sent to the tab the content script is running in
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        // check to see if the message received is something that needs to be acted on
+        if (request.todo === "newWidth") {
+            // pull the width data from the message
+            const newWidth = request.newWidth;
+            // set the style attribute of :root to override the MAIN.css value for --ptwidth
+            document.documentElement.style.setProperty("--ptwidth", newWidth + "px");
+            // send a response to avoid errors in popup.js
+            sendResponse("newWidth");
+        } else if (request.todo === "colorChanger") {
+            // pull the colorChanger data from the message
+            const colorChanger = request.colorChanger;
+            // set the style attribute of :root to override the MAIN.css value for --page
+            document.documentElement.style.setProperty("--page", colorChanger);
+            // send a response to avoid errors in popup.js
+            sendResponse("colorChanger");
+        }
+    });
 });
--- POPUP.js
+++ POPUP.js
@@ -1,62 +1,107 @@
-// Unchanged
-$(document).ready(function () {
-  $(".reset").click(function () {
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      var activeTab = tabs[0];
-      chrome.scripting.removeCSS({
-        target: { tabId: activeTab.id },
-        files: ["MAIN.css"],
-      });
+document.body.onload = function() {
+    chrome.storage.sync.get("newWidth", function(items) {
+        if (!chrome.runtime.error) {
+            console.log(items);
+            document.getElementById("newWidth").value = items.newWidth;
+            document.getElementById("slider").value = items.newWidth;
+        }
     });
-  });
+    chrome.storage.sync.get("colorChanger", function(items) {
+        if (!chrome.runtime.error) {
+            console.log(items);
+            document.getElementById("colorChanger").value = items.colorChanger;
+        }
+    });
+}
 
-  $(".format").click(function () {
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      var activeTab = tabs[0];
-      chrome.scripting.insertCSS({
-        target: { tabId: activeTab.id, allFrames: true },
-        files: ["MAIN.css"],
-      });
-      /*chrome.tabs.sendMessage(activeTab.id, {"buttonclicked": "wider"});*/
+document.onchange = function() {
+    var newWidth = document.getElementById("newWidth").value;
+    var colorChanger = document.getElementById("colorChanger").value;
+    chrome.storage.sync.set({
+        "newWidth": newWidth,
+        "colorChanger": colorChanger,
+    }, function() {
+        if (chrome.runtime.error) {
+            console.log("Runtime error.");
+        }
     });
-  });
+}
+
+$(document).ready(function() {
+    $(".reset").click(function() {
+        chrome.tabs.query({
+            currentWindow: true,
+            active: true
+        }, function(tabs) {
+            var activeTab = tabs[0];
+            chrome.scripting.removeCSS({
+                target: {
+                    tabId: activeTab.id
+                },
+                files: ["MAIN.css"],
+            });
+        });
+    });
+
+    $(".format").click(function() {
+        chrome.tabs.query({
+            currentWindow: true,
+            active: true
+        }, function(tabs) {
+            var activeTab = tabs[0];
+            chrome.scripting.insertCSS({
+                target: {
+                    tabId: activeTab.id,
+                    allFrames: true
+                },
+                files: ["MAIN.css"],
+            });
+        });
+    });
 });
 
-// Changed
-$(function () {
-  // color picker listener
-  $('#color-changer').on('input', function() {
-	const color = $(this).val();
-	chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { todo: "changeColor", newColor: color });
+$(function() {
+    // colorChanger listener
+    $('#colorChanger').on('input', function() {
+        const colorChanger = $(this).val();
+        chrome.tabs.query({
+            currentWindow: true,
+            active: true
+        }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                todo: "colorChanger",
+                colorChanger: colorChanger
+            });
+        });
     });
-  });
-  // text input listener
-  $("#newWidth").on("change paste keyup", function () {
-    const width = $(this).val();
-    // update slider
-    $("#slider").val(width);
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { todo: "changeWidth", newWidth: width });
+    // text input listener
+    $("#newWidth").on("change paste keyup", function() {
+        const newWidth = $(this).val();
+        // update slider
+        $("#slider").val(newWidth);
+        chrome.tabs.query({
+            currentWindow: true,
+            active: true
+        }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                todo: "newWidth",
+                newWidth: newWidth
+            });
+        });
     });
-  });
-  // listener for change button press
-  // button might not be needed anymore because of the text input listener above
-  $("#btnChange").click(function () {
-    const width = $("#newWidth").val();
-    // update slider
-    $("#slider").val(width);
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { todo: "changeWidth", newWidth: width });
+    // listener for slider changes
+    $("#slider").on("input", function() {
+        const newWidth = $("#slider").val();
+        // update text box
+        $("#newWidth").val(newWidth);
+        chrome.tabs.query({
+            currentWindow: true,
+            active: true
+        }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                todo: "newWidth",
+                newWidth: newWidth
+            });
+        });
     });
-  });
-  // listener for slider changes
-  $("#slider").on("input", function () {
-    const width = $("#slider").val();
-    // update text box
-    $("#newWidth").val(width);
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { todo: "changeWidth", newWidth: width });
-    });
-  });
 });
